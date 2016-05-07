8bd94f9666d837ca00f5b80fa9bf68cea9d9556f
Thijs Vogels <t.vogels@me.com>
2016-05-07 14:26:05 +0200

    Add BeautifulSoup dependency

--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ setup(name='svgmath',
       description='LaTeX to SVG with baseline detection',
       author='Thijs Vogels',
       author_email='t.vogels@me.com',
+      url='https://github.com/tvogels/svgmath',
       license='MIT',
+      install_requires=[
+        'bs4'
+      ],
       packages=['svgmath'])
